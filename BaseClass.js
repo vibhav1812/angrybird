72dfd65ad7440bf795e40662ac17ae464993a79c
Ninitha <67736136+Ninitha06@users.noreply.github.com>
2020-10-16 22:51:11 +0530

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -1,9 +1,8 @@
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            'restitution':0.4,
+            'friction':0.5,
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
--- sketch.js
+++ sketch.js
@@ -16,24 +16,23 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-
-
-    box1 = new Box(800,520,70,70);
-    box2 = new Box(1000,520,70,70);
+    ground = new Ground(600,590,1200,20);
+    
+    box1 = new Box(800,540,70,70);
+    box2 = new Box(1000,540,70,70);
     pig1 = new Pig(900, 540);
     log1 = new Log(900,500,280, PI/2);
 
-    box3 = new Box(800,460,70,70);
-    box4 = new Box(1000,460,70,70);
-    pig2 = new Pig(900, 480);
+    box3 = new Box(800,450,70,70);
+    box4 = new Box(1000,450,70,70);
+    pig2 = new Pig(900, 450);
 
-    log3 =  new Log(900,400,280, PI/2);
+    log3 =  new Log(900,410,280, PI/2);
 
-    box5 = new Box(900,400,70,70);
+    box5 = new Box(900,360,70,70);
     log4 = new Log(840,360,150, PI/7);
     log5 = new Log(960,360,150, -PI/7);
-
+   
     bird = new Bird(200,200);
 
 }
